ccc4075f69e2e8e897e7c4907ff2bfad4b0810ab
lizhongzhen11 <295630519@qq.com>
2019-11-04 19:55:22 +0800

    --call

--- 30s.js
+++ 30s.js
@@ -16,3 +16,22 @@ const ary = (fn, n) => (...args) => fn(...args.slice(0, n));
 // 比较前两个数大小
 const firstTwoMax = ary(Math.max, 2);
 firstTwoMax(...[1, 2, 3]); // 2  必须用扩展预算符！！！
+
+
+
+/**
+ * 
+ * @param {*} key 
+ * @param  {...any} args
+ * @description 其实就是把函数放到对象的属性中，根据对象的属性名传参调用对应的函数
+ * 简直就是为策略模式量身打造的！！！ 
+ */
+const call = (key, ...args) => context => context[key](...args);
+
+const callObj = {
+  test(...args) {
+    console.log(...args);
+  }
+}
+const callTest = call('test', ...[1, 2, 3]);
+callTest(callObj); // 1,2,3
